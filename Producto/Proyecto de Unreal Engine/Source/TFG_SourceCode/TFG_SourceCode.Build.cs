0da76a6fd15937cbc95dde61fccd907cf11d418b
eespunes <erik.espunyes7@outlook.com>
2021-04-15 10:21:29 +0200

    Login and register done

--- Producto/Proyecto de Unreal Engine/Source/TFG_SourceCode/TFG_SourceCode.Build.cs
+++ Producto/Proyecto de Unreal Engine/Source/TFG_SourceCode/TFG_SourceCode.Build.cs
@@ -8,7 +8,7 @@ public class TFG_SourceCode : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG","AIModule" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","UMG","AIModule","HTTP","JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
