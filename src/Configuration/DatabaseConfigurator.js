c9407f74e42593dcb76854657e79d02bff3ec8c0
eespunes <erik.espunyes7@outlook.com>
2021-06-09 18:10:36 +0200

    Revert "Merge remote-tracking branch 'origin/s_develop' into documentation"
    This reverts commit 255e8e3579abdbd22b236a4e6df84d5a38d23391, reversing
    changes made to 200caf13cbdd4ffce69aad5d9a8b3bf81b2575a6.

--- src/Configuration/DatabaseConfigurator.js
+++
@@ -1,7 +0,0 @@
-const port = "5432"
-const host = "ec2-34-252-251-16.eu-west-1.compute.amazonaws.com"
-const user = "cdyruewovkodvl"
-const database = "d6963cla9u54hl"
-const password = "b37bda0455e02f535300ec3f8909bf5f99212fcf8d29e1059d7d33156eacfdf6"
-
-module.exports = {port, host, user, database, password}
