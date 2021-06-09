18eb63034c3840cbd26c0c70ef92d13efb0f3099
eespunes <erik.espunyes7@outlook.com>
2021-06-09 18:15:27 +0200

    Bye

--- Producto/Proyecto de Unreal Engine/Source/TFG_sourceCode/TFG_sourceCode.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TFG_sourceCode.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TFG_sourceCode, "TFG_sourceCode" );
